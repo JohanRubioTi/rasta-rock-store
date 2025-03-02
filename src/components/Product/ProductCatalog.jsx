35ab9354c6d77910268d309b592506666f9dd6aa
johanrubioti <johanrubioti@gmail.com>
2025-03-01 19:28:54 -0500

    Center back button icon and text vertically

--- src/components/Product/ProductCatalog.jsx
+++ src/components/Product/ProductCatalog.jsx
@@ -67,7 +67,7 @@ const ProductCatalog = () => {
 
           <div className="container mx-auto p-4">
             <div className="mb-4">
-              <Link to="/" className="nav-link inline-flex">
+              <Link to="/" className="nav-link inline-flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-5 h-5 mr-2">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                 </svg>
